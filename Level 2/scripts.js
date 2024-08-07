93436a087ef3282822d50b81f0b00dc125118979
Christian Green <ianyeetus14@gmail.com>
2024-08-07 18:32:48 -0400

    added more to the imageheightwidth function and outlined a generate arrows function

--- Level 2/scripts.js
+++ Level 2/scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   let arrows = [];
   let screenDimensions = [];
+  let positions = [];
 
   function gcd(a, b) {
     return b === 0 ? a : gcd(b, a % b);
@@ -18,11 +19,12 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function fractionAspectRatio() {
-    const divisor = gcd(Math.round(screen.availHeight / 10) * 10, Math.round(screen.availWidth / 10) * 10);
+    const divisor = gcd(screen.availHeight, screen.availWidth);
     const width = screen.availWidth / divisor;
     const height = screen.availHeight / divisor;
-    return {width, height};
+    return {width, height}; 
   }
+
   function calculateImageHeightWidth() {
     const minElements = 40;
     const maxElements = 80;
@@ -40,12 +42,36 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
 
-    return{ widthImages, heightImages };
+    return { width, height };
   }
   
+  function makePositions() {
+    for (let x = 0; x < calculateImageHeightWidth()[0]; x++) {
+      for (let y = 0; y < calculateImageHeightWidth()[1]; y++) {
+        positions.push({ x: x, y: y });
+      }
+    }
+
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+
+    shuffleArray(positions);
+  }
+
+  function generateArrows() {
+    makePositions();
+    document.img.style = `width: ${screen.availWidth/calculateImageHeightWidth()[0]}px; height:
+    ${screen.availHeight/calculateImageHeightWidth()[1]}px`;
+    for (let i = 0; i < positions.length; i++)
+      arrows.push(new Arrow())
+
+  }
+
   const result = calculateImageHeightWidth();
   console.log(result);
-  
-  
-
+  generateArrows();
 });
