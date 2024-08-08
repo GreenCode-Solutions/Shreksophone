ec33075363e7c612ab8d142a167c3ccd91367620
Christian Green <ianyeetus14@gmail.com>
2024-08-08 16:06:49 -0400

    added arrow class to level two styles.css, got \n rid of obsolete function in js, and added on to \n generateArrows function by adding the img DOM \n with arrow class.

--- Level 2/scripts.js
+++ Level 2/scripts.js
@@ -1,53 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
   let arrows = [];
-  let screenDimensions = [];
+  let screenDimensions = [fractionAspectRatio()[0],fractionAspectRatio()[1]];
   let positions = [];
-
-  function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
-  }
+  let imgDimensions = [calculateImageDimensions()[0], calculateImageDimensions()[1]];
 
 
   class Arrow {
-    constructor(posX, posY, PointsTo = null, tilt, arrowDOM = null){
-      this.posX = posX;
-      this.posY = posY;
+    constructor(X, Y, PointsTo = [null, null], tilt = null, arrowDOM){
+      this.posX = X;
+      this.posY = Y;
       this.next = PointsTo;
       this.tilt = tilt; 
       this.arrowDOM = arrowDOM;
     }
   }
 
-  function fractionAspectRatio() {
+  function fractionAspectRatio() {  
+    function gcd(a, b) {
+      return b === 0 ? a : gcd(b, a % b);
+    }
+
     const divisor = gcd(screen.availHeight, screen.availWidth);
     const width = screen.availWidth / divisor;
     const height = screen.availHeight / divisor;
-    return {width, height}; 
+    return { width, height }; 
   }
 
-  function calculateImageHeightWidth() {
-    const minElements = 40;
-    const maxElements = 80;
-    const maxRatio = 1.05;
-    const minRatio = .95;
+  function calculateImageDimensions() {
     const {width, height} = fractionAspectRatio();
     let   {widthImages, heightImages} = 0;  
-    let correctRatio = false;
-
-    for (let i = 0; correctRatio == false; i++) {
-      widthImages = screen.availWidth/width;
-      heightImages = screen.availHeight/height;
-      if (maxRatio > widthImages/heightImages > minRatio && minElements < width*height < maxElements) {
-        correctRatio = true;
-      }
-    }
-
-    return { width, height };
+    widthImages = screen.availWidth/width;
+    heightImages = screen.availHeight/height;
+    return { widthImages, heightImages };
   }
   
   function makePositions() {
-    for (let x = 0; x < calculateImageHeightWidth()[0]; x++) {
-      for (let y = 0; y < calculateImageHeightWidth()[1]; y++) {
+    for (let x = 0; x < screenDimensions[0]; x++) {
+      for (let y = 0; y < screenDimensions[1]; y++) {
         positions.push({ x: x, y: y });
       }
     }
@@ -64,14 +53,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function generateArrows() {
     makePositions();
-    document.img.style = `width: ${screen.availWidth/calculateImageHeightWidth()[0]}px; height:
-    ${screen.availHeight/calculateImageHeightWidth()[1]}px`;
-    for (let i = 0; i < positions.length; i++)
-      arrows.push(new Arrow())
+    for (let i = 0; i < positions.length; i++) {
+      let arrowDOM = document.createElement("img")
+      arrowDOM.class = 'arrow';
+      arrowDOM.style.left = `${imgDimensions[0] * positions[i][0]}px`;
+      arrowDOM.style.top = `${imgDimensions[1] * positions[i][1]}px`;
+      if (arrows.length == 0) {
+        arrows.push(new Arrow(X=positions[i][0], Y=positions[i][1], arrowDOM=arrowDOM));
+      } else {
+        
+      }
+      
+      
+    }
 
   }
 
-  const result = calculateImageHeightWidth();
+  const result = calculateImageDimensions();
   console.log(result);
   generateArrows();
 });
